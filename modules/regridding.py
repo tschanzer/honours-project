df5b1b77a05173287bc2c3b1fc714f086681734d
tschanzer <76029848+tschanzer@users.noreply.github.com>
2023-07-02 01:12:46 +1000

    Rewrite regridding module from scratch

--- modules/regridding.py
+++ modules/regridding.py
@@ -1,277 +1,146 @@
-"""Wrappers of xesmf.Regridder for arbitrary data."""
+"""Utilities for conservative rectilinear regridding."""
 
 import numpy as np
-import xesmf as xe
+import scipy as sp
 import xarray as xr
-import warnings
 
 
-def bounds(grid):
-    """
-    Calculates cell bounds for a 1D coordinate grid.
+class Regridder:
+    """Regridding object for N-D rectilinear grids."""
 
-    Assumes that points on the coordinate grid are the midpoints of
-    the cells and that the first and last cells have equal width.
-    If the grid has even length, we additionally assume that the
-    boundary between the first and second grid points is halfway
-    between them (otherwise, the problem would be underdetermined).
-
-    Note: this is not, in general, the same as assuming that the cell
-    bounds are halfway between points on the coordinate grid.
-
-    Args:
-        grid: 1D coordinate grid.
-
-    Returns:
-        Array of cell edges with length x.size + 1.
-    """
-
-    mat = np.eye(grid.size + 1) + np.eye(grid.size + 1, k=1)
-    mat[-1, 0] = 1
-    rhs = np.concatenate([2*grid, [grid[0] + grid[-1]]])
-
-    if grid.size % 2 == 1:
-        # the matrix is singular
-        mat[0, 0] = 0
-        rhs[0] = 0.5*(grid[0] + grid[1])
-
-    return np.linalg.solve(mat, rhs)
-
-
-class Regridder2D(xe.Regridder):
-    """Wrapper of xesmf.Regridder for data with different coordinate names."""
-
-    def __init__(self, highres, lowres, coords=('x', 'z')):
+    def __init__(self, source, target, dims, limits=None):
         """
-        Creates a 1st order conservative regridding object.
+        Creates the regridding object.
 
         Args:
-            highres: High-resolution xarray.Dataset or DataArray.
-            lowres: Low-resolution xarray.Dataset or DataArray.
-            coords: 2-tuple of regridding coordinate names.
-
-        Returns:
-            Modified xesmf.Regridder object that regrids data from the
-            `highres` grid onto the `lowres` grid.
+            source: Fine-resolution xarray.Dataset or xarray.DataArray.
+            target: Coarse-resolution xarray.Dataset or xarray.DataArray.
+            dims: List of names of dimensions to be regridded.
+            limits: Dict of grid limits (see `bounds`).
         """
 
-        self.coords = coords
-        self.scale_factors = [np.abs(lowres[coord]).max() for coord in coords]
-        highres = self._prep_regrid(highres)
-        lowres = self._prep_regrid(lowres)
-        with warnings.catch_warnings():
-            warnings.filterwarnings(
-                'ignore', message=r'Latitude is outside of \[-90, 90\]')
-            super().__init__(highres, lowres, 'conservative')
+        self.dims = dims
+        self.source_shape = tuple(source[dim].size for dim in dims)
+        self.target_shape = tuple(target[dim].size for dim in dims)
+        self.target_coords = target.coords
+        self.weights = {}
+        if limits is None:
+            limits = {}
 
-    def __call__(self, data):
+        for dim in dims:
+            # Compute the weight matrix along each dimension
+            dim_limits = limits.get(dim)
+            source_bounds = bounds(source[dim].data, limits=dim_limits)
+            target_bounds = bounds(target[dim].data, limits=dim_limits)
+            weight = compute_weights_1d(source_bounds, target_bounds)
+            self.weights[dim] = weight
+
+    def __call__(self, grid):
         """
-        Coarse-grains high-resolution data.
+        Regrids data.
 
         Args:
-            data: High-resolution xarray.Dataset or DataArray.
-                to be coarse-grained.
+            grid: Fine-resolution xarray.Dataset or xarray.DataArray.
 
         Returns:
-            Coarse-grained version of the input.
+            Regridded version of the input.
         """
 
-        data = self._coords_to_latlon(data)
-        data = self._check_dim_order(data)
-        result = super().__call__(data)
-        return self._latlon_to_coords(result)
+        # Regrid the data one dimension at a time
+        for dim in self.dims:
+            # drop any non-index coordinates that depend on `dim`
+            for coord in grid.coords:
+                if coord != dim and dim in grid.coords[coord].dims:
+                    grid = grid.reset_coords(coord, drop=True)
 
-    def _coords_to_latlon(self, data):
-        """
-        Renames coordinates for regridding by xESMF.
+            # Regrid along `dim`, broadcasting over other dimensions
+            grid = xr.apply_ufunc(
+                lambda x: self.weights[dim] @ x,  # pylint: disable=W0640
+                grid,
+                input_core_dims=[[dim]],
+                output_core_dims=[[dim]],
+                exclude_dims={dim},
+                vectorize=True,
+            )
+            # insert the coarse coordinates
+            grid = grid.assign_coords({dim: self.target_coords[dim].data})
 
-        Args:
-            data: xarray.Dataset or DataArray.
-
-        Returns:
-            xarray.Dataset or DataArray with renamed coordinates.
-        """
-
-        data = data.rename({self.coords[0]: 'lat', self.coords[1]: 'lon'})
-        # we need to rescale the coordinates because xESMF doesn't like
-        # it when abs(lon) > 180 or abs(lat) > 90 ...
-        data['lat'] = data['lat']/self.scale_factors[0]
-        data['lon'] = data['lon']/self.scale_factors[1]
-        return data
-
-    def _latlon_to_coords(self, data):
-        """
-        Undoes the action of _coords_to_latlon.
-
-        Args:
-            data: xarray.Dataset or DataArray with renamed coordinates.
-
-        Returns:
-            xarray.Dataset or DataArray with original coordinate names.
-        """
-
-        # first undo the rescaling
-        data['lat'] = data['lat']*self.scale_factors[0]
-        data['lon'] = data['lon']*self.scale_factors[1]
-        return data.rename({'lat': self.coords[0], 'lon': self.coords[1]})
-
-    def _prep_regrid(self, data):
-        """
-        Prepares an array for regridding by xESMF.
-
-        Args:
-            data: xarray.Dataset or DataArray.
-
-        Returns:
-            xarray.Dataset or DataArray with renamed coordinates and
-            added coordinate bounds.
-        """
-
-        data = self._coords_to_latlon(data)
-        data = self._check_dataset(data)
-        data = data.assign_coords({
-            'lat_b': bounds(data.lat.data),
-            'lon_b': bounds(data.lon.data),
-        })
-        return data
-
-    def _check_dim_order(self, data):
-        """
-        Ensures the dimensions are in the right order.
-
-        Args:
-            data: xarray.Dataset or DataArray with 'lat', and 'lon'
-                coordinates.
-
-        Returns:
-            C-contiguous copy of the input with 'lat' and 'lon' as the
-            last two dimensions.
-        """
-
-        dims = list(data.dims)
-        if dims[-2:] == ['lat', 'lon']:
-            return data
-
-        other_coords = [dim for dim in dims if dim not in ['lat', 'lon']]
-        data = data.transpose(*other_coords, 'lat', 'lon')
-        # The data must be made C-contiguous to optimise performance
-        data = data.astype(
-            data.dtypes if isinstance(data, xr.Dataset) else data.dtype,
-            order='C',
-        )
-        return data
-
-    def _check_dataset(self, data):
-        """
-        Converts DataArrays to coordinate Datasets.
-
-        This is needed because xarray cannot assign coordinates with
-        new dimensions to DataArrays.
-
-        Args:
-            data: xarray.Dataset or DataArray with 'lat' and 'lon'
-                coordinates.
-
-        Returns:
-            An xarray.Dataset with the coordinate data if the input
-            is a DataArray, and the input otherwise.
-        """
-
-        if isinstance(data, xr.DataArray):
-            data = xr.Dataset({'lat': data.lat, 'lon': data.lon})
-
-        return data
-
-
-class Regridder1D(Regridder2D):
-    """Wrapper of xesmf.Regridder for 1D regridding."""
-
-    def __init__(self, highres, lowres, coord='t'):
-        """
-        Creates a 1st order conservative regridding object.
-
-        Args:
-            highres: High-resolution xarray.Dataset or DataArray.
-            lowres: Low-resolution xarray.Dataset or DataArray.
-            coord: Name of regridding coordinate.
-
-        Returns:
-            Modified xesmf.Regridder object that regrids data from the
-            `highres` grid onto the `lowres` grid.
-        """
-
-        self.coord = coord
-        self.dummy_coord = 'dummy_' + coord
-        highres = self._add_dummy_coord(highres)
-        lowres = self._add_dummy_coord(lowres)
-        super().__init__(highres, lowres, (self.coord, self.dummy_coord))
-
-    def _add_dummy_coord(self, data):
-        """Adds a length-1 dummy coordinate."""
-        return data.expand_dims({self.dummy_coord: [1/2]}, axis=-1)
-
-    def _remove_dummy_coord(self, data):
-        """Removes the length-1 dummy coordinate."""
-        return data.squeeze(self.dummy_coord, drop=True)
-
-    def _coords_to_latlon(self, data):
-        """Wrapper of Regridder2D._coords_to_latlon for 1D regridding."""
-        data = self._add_dummy_coord(data)
-        return super()._coords_to_latlon(data)
-
-    def _latlon_to_coords(self, data):
-        """Wrapper of Regridder2D._latlon_to_coords for 1D regridding."""
-        data = super()._latlon_to_coords(data)
-        return self._remove_dummy_coord(data)
-
-    def _prep_regrid(self, data):
-        """Equivalent of Regridder2D._prep_regrid for 1D regridding."""
-        data = super()._coords_to_latlon(data)
-        data = self._check_dataset(data)
-        data = data.assign_coords({
-            'lat_b': bounds(data.lat.data),
-            'lon_b': [0., 1.],
-        })
-        return data
-
-
-class Regridder3D:
-    """Wrapper of xesmf.Regridder for 3D regridding."""
-
-    def __init__(self, highres, lowres, coords=('x', 'z', 't')):
-        """
-        Creates a 1st order conservative regridding object.
-
-        Args:
-            highres: High-resolution xarray.Dataset or DataArray.
-            lowres: Low-resolution xarray.Dataset or DataArray.
-            coord: Name of regridding coordinates. The first two will
-                be regridded first, followed by the third.
-
-        Returns:
-            Modified xesmf.Regridder object that regrids data from the
-            `highres` grid onto the `lowres` grid.
-        """
-
-        self.regridder2d = Regridder2D(highres, lowres, coords[:2])
-        self.regridder1d = Regridder1D(highres, lowres, coords[-1])
-
-    def __call__(self, data):
-        """
-        Coarse-grains high-resolution data.
-
-        Args:
-            data: High-resolution xarray.Dataset or DataArray.
-                to be coarse-grained.
-
-        Returns:
-            Coarse-grained version of the input.
-        """
-
-        return self.regridder1d(self.regridder2d(data))
+        return grid
 
     def __repr__(self):
         """Returns information about the regridder."""
-        repr_2d = self.regridder2d.__repr__()
-        repr_1d = self.regridder1d.__repr__()
-        return repr_2d + '\n\n' + repr_1d
+
+        source_shape = dict(zip(self.dims, self.source_shape))
+        target_shape = dict(zip(self.dims, self.target_shape))
+        info = (
+            'Conservative Regridder\n'
+            f'  Input grid shape: {source_shape}\n'
+            f'  Output grid shape: {target_shape}'
+        )
+        return info
+
+
+def compute_weights_1d(source, target):
+    """
+    Regridding weight matrix for 1d grids.
+
+    weight_ij = (
+        (overlapping length between target interval i and source interval j)
+        / (length of target interval i)
+    )
+
+    Args:
+        source: Array of boundaries between points on the source grid.
+        target: Array of boundaries between points on the target grid.
+    """
+
+    # promote grids to 2d to allow broadcasting
+    source = np.atleast_2d(source)  # source has index j
+    target = np.atleast_2d(target).T  # target has index i
+
+    # define shifted arrays for code readability
+    source_j = source[:, :-1]
+    target_i = target[:-1, :]
+    source_jplus1 = source[:, 1:]
+    target_iplus1 = target[1:, :]
+
+    # compute overlap length between every possible pair of intervals
+    overlap = (
+        np.minimum(source_jplus1, target_iplus1)
+        - np.maximum(source_j, target_i)
+    )
+    overlap = np.maximum(overlap, 0)  # ensure it is non-negative
+
+    # find the length of each target interval
+    target_lengths = (
+        np.minimum(target_iplus1, source[0, -1])
+        - np.maximum(target_i, source[0, 0])
+    )
+    # if the target interval does not overlap with the source grid,
+    # set its length to NaN
+    target_lengths[target_lengths <= 0] = np.nan
+
+    # return the overlap fraction as a sparse matrix
+    return sp.sparse.csr_array(overlap/target_lengths)
+
+
+def bounds(grid, limits=None):
+    """
+    Estimates boundaries between 1D grid points.
+
+    Args:
+        grid: 1D array of grid points.
+        limits: Optional list specifying lower and upper boundaries.
+
+    Returns:
+        Array of estimated boundaries, with length len(grid) + 1.
+        Boundaries are assumed to be the midpoints between grid points.
+    """
+
+    if limits is None:
+        # choose first and last boundaries so the first and last grid
+        # points are at the centres of their respective intervals
+        limits = [2*grid[0] - grid[1], 2*grid[-1] - grid[-2]]
+
+    midpoints = (grid[:-1] + grid[1:])/2
+    return np.concatenate([[limits[0]], midpoints, [limits[1]]])
