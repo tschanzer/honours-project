b4baf841cb04d5b29f04f5c6e85546128a53394f
tschanzer <76029848+tschanzer@users.noreply.github.com>
2023-10-28 21:18:33 +1100

    Work on tendency histograms

--- modules/tools.py
+++ modules/tools.py
@@ -26,14 +26,15 @@ def tendency(state_t, state_tplusdt):
     return tend.assign_coords({'t': state_t.t})
 
 
-def insert_bc(data, bottom, top, aspect):
+def insert_bc(data, bc, aspect):
     """
     Inserts the boundary values into a DataArray.
 
     Args:
-        data: xr.DataArray.
-        bottom: z=0 boundary condition (pass None to omit).
-        top: z=1 boundary condition (pass None to omit).
+        data: xarray.DataArray or xarray.Dataset.
+        bc: If data is a DataArray, a tuple (bottom, top) of bottom/top
+            boundary values. If data is a Dataset, a dict mapping
+            variable names to tuples of the above form.
         aspect: Domain aspect ratio.
 
     Returns:
@@ -41,14 +42,29 @@ def insert_bc(data, bottom, top, aspect):
     """
 
     # z=0 value
-    z_bc = xr.DataArray([bottom], coords={'z': [0.]})
-    _, z_bc = xr.broadcast(data.isel(z=0), z_bc)
-    data = xr.concat([z_bc, data], dim='z')
+    if isinstance(data, xr.DataArray):
+        bottom = xr.DataArray([bc[0]], coords={'z': [0.]})
+    elif isinstance(data, xr.Dataset):
+        bottom = xr.Dataset({
+            var: xr.DataArray([val[0]], coords={'z': [0.]})
+            for var, val in bc.items()
+        })
+    else:
+        raise ValueError('Invalid data type.')
+
+    _, bottom = xr.broadcast(data.isel(z=0), bottom)
+    data = xr.concat([bottom, data], dim='z')
 
     # z=1 value
-    z_bc = xr.DataArray([top], coords={'z': [1.]})
-    _, z_bc = xr.broadcast(data.isel(z=0), z_bc)
-    data = xr.concat([data, z_bc], dim='z')
+    if isinstance(data, xr.DataArray):
+        top = xr.DataArray([bc[1]], coords={'z': [1.]})
+    else:
+        top = xr.Dataset({
+            var: xr.DataArray([val[1]], coords={'z': [1.]})
+            for var, val in bc.items()
+        })
+    _, top = xr.broadcast(data.isel(z=0), top)
+    data = xr.concat([data, top], dim='z')
 
     # Periodic x value
     x_bc = data.sel(x=0).assign_coords({'x': aspect})
@@ -56,21 +72,18 @@ def insert_bc(data, bottom, top, aspect):
     return data
 
 
-def sample(data, x, z, bottom, top, aspect):
+def sample(data, x, z):
     """
     Samples a field at given points using linear interpolation.
 
     Args:
         data: xr.DataArray.
         x, z: 1D arrays of coordinates for the sampling points.
-        bottom, top: z=0,1 boundary values for the field.
-        aspect: Domain aspect ratio.
 
     Returns:
         1D np.ndarray of field values at sampling points.
     """
 
-    data = insert_bc(data, bottom, top, aspect)
     return sp.interpolate.interpn(
         (data.x.data, data.z.data),
         data.transpose('x', 'z', 't').data,
@@ -83,3 +96,60 @@ def label_subplots(axes):
     if axes.ndim > 1: axes = axes.ravel()
     for i, ax in enumerate(axes):
         ax.set_title(f'({string.ascii_lowercase[i]})', loc='left')
+
+
+def plot_z_dependence(
+        x_data, y_data, axes, z_ranges, n_samples,
+        aspect, sigma, **hist2d_kwargs):
+    """
+    Plot a series of histograms conditioned on z.
+
+    Args:
+        x_data, y_data: xr.DataArray.
+        axes: Array of axes.
+        ranges: List of (z_min, z_max) with length equal to axes.size.
+        n_samples: Number of samples to take in each range.
+        aspect: Domain aspect ratio.
+        sigma: Standard deviation of the Gaussian filter used to smooth
+            the histogram array for plotting contours.
+        hist2d_kwargs: Additional keyword arguments to pass to hist2d.
+    """
+
+    rng = np.random.default_rng(seed=0)
+    meshes = np.empty_like(axes)
+    for i, (ax, (z_min, z_max)) in enumerate(zip(axes.ravel(), z_ranges)):
+        x_sample = rng.uniform(0, aspect, n_samples)
+        z_sample = rng.uniform(z_min, z_max, n_samples)
+        x_data_sample = sample(x_data, x_sample, z_sample).ravel()
+        y_data_sample = sample(y_data, x_sample, z_sample).ravel()
+        hist, x, y, meshes.flat[i] = ax.hist2d(
+            x_data_sample, y_data_sample, norm='log', **hist2d_kwargs,
+        )
+        hist_contour(ax, x, y, hist, sigma)
+        ax.set(title=f'$z \\in [{z_min:.3f}, {z_max:.3f}]$')
+
+    return meshes
+
+
+def hist_contour(axes, x, y, hist, sigma):
+    """
+    Plot smooth contours on a 2D histogram.
+
+    Args:
+        axes: Axes on which to draw the contours.
+        x, y: Bin edges of the histogram.
+        hist: Histogram array.
+        sigma: Standard deviation of the Gaussian filter used to smooth
+            the histogram array.
+
+    Returns:
+        QuadContourSet.
+    """
+
+    hist = sp.ndimage.gaussian_filter(hist, sigma=sigma)
+    hist[hist >= 1] = np.log10(hist[hist >= 1])
+    contour = axes.contour(
+        (x[:-1] + x[1:])/2, (y[:-1] + y[1:])/2, hist.T,
+        colors='k', linewidths=0.5, levels=np.arange(1, hist.max(), 0.5)
+    )
+    return contour
